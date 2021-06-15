bc7cd0c357eaa0021a3b858bc154089b6a3afd24
JudgeFudge19 <dremukare@gmail.com>
2021-06-15 21:43:17 +0000

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,11 +10,10 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp, flag;
+	int temp;
 
 	for (i = 0; i < size && flag; i++)
 	{
-		flag = 0;
 		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -22,11 +21,8 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				flag = 1;
 				print_array(array, size);
 			}
 		}
-		if (flag == 0)
-			break;
 	}
 }
