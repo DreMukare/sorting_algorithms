901137120b7f125db470a8a0df8147e12005a2ad
JudgeFudge19 <dremukare@gmail.com>
2021-06-16 13:40:28 +0000

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,14 +16,14 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size; i++)
 	{
 		min_el_idx = i;
-		for (j = i + 1; j < size - 1; j++)
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < arr[min_el_idx])
+			if (array[j] < array[min_el_idx])
 				min_el_idx = j;
 		}
-		temp = arr[i];
-		arr[i] = arr[min_el_idx];
-		arr[min_el_idx] = temp;
+		temp = array[i];
+		array[i] = array[min_el_idx];
+		array[min_el_idx] = temp;
 		print_array(array, size);
 	}
 }
